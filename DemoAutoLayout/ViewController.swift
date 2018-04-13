7e6ea9e73ebaae875f9fd48d43236b3a25a94642
Chao Shin <chao.shin@gmail.com>
2018-04-13 22:06:53 +0800

    Initial Commit

---
+++ DemoAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoAutoLayout
+//
+//  Created by Chao Shin on 2018/4/13.
+//  Copyright © 2018 Chao Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
